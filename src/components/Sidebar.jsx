1366e15b0ba4d168ab070b02bdbfc1e150f0a895
SarojWasti <sarozwosti@gmail.com>
2024-10-05 20:18:11 -0400

    chore: fix sidebar ui

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -26,25 +26,25 @@ const Sidebar = () => {
                     <ul className="space-y-10">
                         <li className="custom-list">
                             <a onClick={() => navigate("/dashboard")} className="flex items-center cursor-pointer hover:text-red-500 transition-colors duration-300">
-                                <FontAwesomeIcon icon={faChartPie} className="text-lg" />
+                                <FontAwesomeIcon icon={faChartPie} className="md:text-lg text-2xl" />
                                 <span className="ml-4 hidden md:inline">Dashboard</span>
                             </a>
                         </li>
                         <li className="custom-list">
                             <a onClick={() => navigate('/dashboard/expenses')} className="flex items-center cursor-pointer hover:text-green-500 transition-colors duration-300">
-                                <FontAwesomeIcon icon={faMoneyBillWave} className="text-lg" />
+                                <FontAwesomeIcon icon={faMoneyBillWave} className="md:text-lg text-2xl" />
                                 <span className="ml-4 hidden md:inline">Expenses</span>
                             </a>
                         </li>
                         <li className="custom-list">
                             <a onClick={() => navigate('/dashboard/messages')} className="flex items-center cursor-pointer hover:text-blue-500 transition-colors duration-300">
-                                <FontAwesomeIcon icon={faEnvelope} className="text-lg" />
+                                <FontAwesomeIcon icon={faEnvelope} className="md:text-lg text-2xl" />
                                 <span className="ml-4 hidden md:inline">Messages</span>
                             </a>
                         </li>
                         <li className="custom-list">
                             <a className="flex items-center cursor-pointer hover:text-yellow-500 transition-colors duration-300">
-                                <FontAwesomeIcon icon={faCog} className="text-lg" />
+                                <FontAwesomeIcon icon={faCog} className="md:text-lg text-2xl" />
                                 <span className="ml-4 hidden md:inline">Settings</span>
                             </a>
                         </li>
@@ -52,7 +52,7 @@ const Sidebar = () => {
                     <ul className='mt-24'>
                         <li className="custom-list">
                             <a className="flex items-center cursor-pointer hover:text-red-500 transition-colors duration-300" onClick={() => logOut()}>
-                            <FontAwesomeIcon icon={faSignOut} className="text-lg" />
+                            <FontAwesomeIcon icon={faSignOut} className="md:text-lg text-2xl" />
                             <span className="ml-4 hidden md:inline">Log Out</span>
                         </a>
                         </li>
