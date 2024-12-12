2b0c19770d32bc790b737ec313a20440d717eae1
SarojWasti <sarozwosti@gmail.com>
2024-12-12 02:31:09 -0500

    Improve popup

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,15 +2,19 @@ import Swal from "sweetalert2";
 
 const DisplayMessage = (msgTitle, msgIcon) => {
     Swal.fire({
-      title: `<h2 style="font-size: 24px;">${msgTitle}</h2>`,
+      title: `<strong style="font-size: 20px; color: #333;">${msgTitle}</strong>`,
       icon: msgIcon,
-      width: '200px',  
-      height: '100px',
-      heightAuto: true,  // Maintain automatic height to avoid stretching
-      timer: 1000,  // Auto close after 3 seconds
+      width: '250px',  
+      padding: '15px',
+      heightAuto: true,
+      timer: 1500, 
       timerProgressBar: true,
-      showConfirmButton: false,  // Hide the "OK" button
-      iconColor: '#006400'  // Very dark green (Dark Green in hex)
+      showConfirmButton: false,
+      iconColor: '#006400',
+      background: '#f9f9f9', 
+      position: 'top',  // Changed to top middle
+      toast: true, 
+      grow: false
     });
- };
+  };
 export default DisplayMessage;
